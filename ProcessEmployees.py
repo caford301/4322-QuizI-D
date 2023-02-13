4ed66c03a019ce030974053843a8e419ecc88e4d
Connor <connor_ford1@baylor.edu>
2023-02-13 13:46:47 -0600

    Finished Quiz

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -12,30 +12,31 @@ import csv
 
 #open the file
 
+infile = open('employee_data.csv', 'r')
 
-
-
+employee_file = csv.reader(infile, delimiter=',')
+next(employee_file)
 #create an empty dictionary
 
+employee_dict = dict()
 
 #use a loop to iterate through the csv file
-
-
+for record in employee_file:
     #check if the employee fits the search criteria
+    if record[4] == "CSR":
+        if record[3] == "Marketing":
+            tempKey = record[1] + " " + record[2]
+            employee_dict[tempKey] = float(record[5])
 
-
-    
+for x in employee_dict:
+    print("Manager Name: " + x + " Current Salary: $" + str(employee_dict[x]))
 
 print()
 print('=========================================')
 print()
 
 #iternate through the dictionary and print out the key and value as per printout
-
-
-
-          
-        
-
-        
-    
+for x in employee_dict:
+    employee_dict[x] = employee_dict[x] * 1.1
+    print("Manager Name: " + x + " New Salary: $" +
+          str(round(employee_dict[x], 2)))
